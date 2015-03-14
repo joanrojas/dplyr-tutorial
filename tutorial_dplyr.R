ed010cb91b84b060976b8e9eaa50fa396a5a2441
jrojas <jrojaslombarte@gmail.com>
2015-03-14 22:16:51 +0100

    done

--- tutorial_dplyr.R
+++ tutorial_dplyr.R
@@ -34,7 +34,9 @@ arrange(flights, date, hour, minute)
 arrange(flights, desc(dep_delay))
 arrange(flights, dep_delay - arr_delay)
 names(flights)
+#Creating a new variable speed
 flights <- mutate(flights, speed = dist / (time / 60))
+#-----
 flights <- arrange(flights, desc(speed))
 by_date <- group_by(flights, date)
 filter(summarise(by_date, mean_distance = mean(dist), mean_dep_delay = mean(dep_delay, na.rm= TRUE), over_15 = mean(dep_delay, na.rm = TRUE)>15), over_15 == TRUE)
@@ -46,8 +48,77 @@ filter(summarise(by_date, mean_distance = mean(dist), mean_dep_delay = mean(dep_
 flights %>% group_by(dest) %>% summarise(dep_d = mean(dep_delay, na.rm= TRUE)) %>% arrange(desc(dep_d))
 
 
-#Which flights ( carrier+flight) happen every day? Where do they fly to?
+#Which flights ( carrier+flight number + destination) happen every day? Where do they fly to?
 flights %>% group_by(carrier, flight, dest) %>% summarise(n=n()) %>% filter(n == 365)
 
 
-#
+flights %>% select(carrier,dep_delay) %>% arrange(dep_delay)
+
+flights %>% select(dist,time) %>% mutate(speedy = dist / (time/60))
+
+flights %>% 
+  group_by(dest) %>% 
+  summarise(avg_delay = mean(arr_delay, na.rm = TRUE)) 
+
+#For each carrier, the percentage of fligts cancelled.
+flights %>% group_by(carrier) %>% summarise( Percentage_cancelled = mean(cancelled))
+
+#For each carrier, maximum and minimum departure delay VERY NICE!!
+flights %>% group_by(carrier) %>% 
+  summarise_each(funs(max(., na.rm = TRUE), min(., na.rm = TRUE)), matches("delay"))
+
+flights %>% group_by(carrier,flight,dest) %>%  summarise(times = n()) %>% filter(carrier == "AA" , times == 365)
+
+
+
+#For each day of the year, count the total number of flights
+flights %>%
+  group_by(date) %>%
+  summarise(number_of_flights = n()) %>% arrange(desc(number_of_flights))
+
+#Which destinations have the highest average delays?
+flights %>% 
+  group_by(dest) %>%
+  summarise(average_delay = mean(arr_delay, na.rm = TRUE)) %>%
+  arrange(desc(average_delay)) 
+
+#How do delays vary on the course of the day?
+flights %>%
+  filter(cancelled =="0") %>%
+  group_by(hour,minute) %>%
+  summarise(avg_delay = mean(dep_delay, na.rm = TRUE))
+ 
+#Now combining dplyr and ggplot for rapid exploratory analysis
+
+#1. What is the average dep and arr delay for each carrier?
+#Plotting a scatterplot
+flights %>% 
+  group_by(carrier) %>%
+  summarise_each(funs(mean(., na.rm = TRUE)), matches("delay")) %>%
+  ggplot(aes(x= arr_delay, y= dep_delay, color = carrier)) + 
+  geom_point()
+
+#2. How do dep_delays evolve during the day?
+flights %>%
+  mutate(time_of_day = hour + minute/60) %>%
+  group_by(time_of_day) %>%
+  summarise(dep_delay = mean(dep_delay, na.rm= TRUE), n = n()) %>%
+  arrange(desc(dep_delay)) %>%
+  filter(n >50) %>%
+  ggplot(aes(x = time_of_day , y = dep_delay, size = n)) + 
+  geom_point() +
+  scale_size_area() 
+
+#3. Plot about the percentage of departure delays of each carrier.
+flights %>%
+  group_by(carrier) %>%
+  mutate(is_delayed = dep_delay >= 0, is_arr_delayed = arr_delay >= 0  ) %>%
+  summarise(me_delay = mean(is_delayed, na.rm = TRUE),
+            me_arr_delay = mean(is_arr_delayed, na.rm = TRUE),me_canc = mean(cancelled, na.rm = TRUE)
+            , n = n ()) %>%
+  filter(n >2500) %>%
+  ggplot(aes(x = carrier, y = me_delay, size= n )) + geom_point() + scale_size_area()
+
+
+  
+
